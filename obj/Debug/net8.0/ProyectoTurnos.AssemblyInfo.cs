edb306f609f57db3388467678494ffdb20289062
FelipeGallego <gallegofelipe29@gmail.com>
2024-04-24 14:49:03 -0500

    dsad

--- obj/Debug/net8.0/ProyectoTurnos.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProyectoTurnos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoTurnos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87f58dd0a094acbb2a3c3a6943e5bee9f57c9767")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3289418429e8209e8a43be9a22b29ef34449035")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoTurnos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoTurnos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
